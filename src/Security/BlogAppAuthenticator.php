5ef7c6710bce5934e99512c95c72b1ea337db443
JgPhil <jamingph@gmail.com>
2020-11-18 09:05:06 +0100

    feat: login

--- src/Security/BlogAppAuthenticator.php
+++ src/Security/BlogAppAuthenticator.php
@@ -95,9 +95,7 @@ class BlogAppAuthenticator extends AbstractFormLoginAuthenticator implements Pas
         if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
             return new RedirectResponse($targetPath);
         }
-
-        // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('home'));
     }
 
     protected function getLoginUrl()
